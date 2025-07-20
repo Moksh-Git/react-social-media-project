0c8581d299ccb56c65433af85ef72f06bfee7a01
Moksh-Git <patilmokshadg@gmail.com>
2025-07-20 20:10:01 +0530

    added some components and page switching using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
     <>
-      <Header/>
-      <Footer/>
+      <div className="app-container">
+        <Sidebar selectedTab={selectedTab}/>
+        <div className="content">
+          <Header />
+          {selectedTab == "Home" ? <PostList /> : <CreatePost />}
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
